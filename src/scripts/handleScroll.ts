43a71c43a1ee7dcfcdd85a353bc6d650fad4e26b
Stanislaw Dymny <dymnystan@gmail.com>
2025-03-11 10:49:46 +0100

    action on scroll improved

--- src/scripts/handleScroll.ts
+++ src/scripts/handleScroll.ts
@@ -2,38 +2,55 @@ const toTrackEls: NodeListOf<Element> = document.querySelectorAll(".animate-scro
 const aboutMeSectionEl: Element | null = document.querySelector("#about-me");
 const headerEl: Element | null = document.querySelector("header");
 
-const applyScrollEffectPrefix = "on-scroll:";
+const addClassOnScroll: string = "on-scroll:";
+const removeClassOnScroll: string = "-on-scroll:"
+
+toTrackEls.forEach((el, i) => {
+  const elClassesArr: string[] = [...el.classList];
+  const elClassesToChange = elClassesArr.filter((className) => className.startsWith(addClassOnScroll) || className.startsWith(removeClassOnScroll));
+
+  const classesToAdd = elClassesToChange.filter((className) => !className.startsWith("-"));
+  const classesToRemove = elClassesToChange.filter((className) => className.startsWith("-"));
+
+  const classesToAddScrollStr = classesToAdd.map(className => className.replace(addClassOnScroll, "")).join(" ")
+  const classesToRemoveSCrollStr = classesToRemove.map(className => className.replace(removeClassOnScroll, "")).join(" ")
+
+  toTrackEls[i].setAttribute("class-add-scroll", classesToAddScrollStr);
+  toTrackEls[i].setAttribute("class-Remove-scroll", classesToRemoveSCrollStr);
+})
+
 
 const observer: IntersectionObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    const entryClasses: string[] = [...entry.target.classList];
-    const entryClassesToChange: string[] = entryClasses.filter((className) =>
-      className.startsWith(applyScrollEffectPrefix)
-    );
-    const entryClassesTrimmed: string[] = entryClassesToChange.map((className) =>
-      className.replace(applyScrollEffectPrefix, "")
-    );
-    const entryClassesRemove: string[] = entryClassesTrimmed
-      .filter((className) => className.startsWith("-"))
-      .map((className) => className.replace("-", ""));
-    const entryClassesAdd: string[] = entryClassesTrimmed.filter(
-      (className) => !className.startsWith("-")
-    );
-
     if (entry.isIntersecting) {
-      entryClassesAdd.forEach((className) => {
+      const classesToRemoveStr: string | null = entry.target.getAttribute("class-add-scroll")
+      const classesToAddStr: string | null = entry.target.getAttribute("class-add-scroll")
+
+      const classesToAdd = classesToAddStr?.split(" ");
+      const classesToRemove = classesToRemoveStr?.split(" ");
+
+      classesToAdd?.forEach((className) => {
         entry.target.classList.add(className);
-      });
-      entryClassesRemove.forEach((className) => {
+      })
+
+      classesToRemove?.forEach((className) => {
         entry.target.classList.remove(className);
-      });
+      })
+
     } else {
-      entryClassesAdd.forEach((className) => {
+      const classesToAddStr: string | null = entry.target.getAttribute("class-add-scroll")
+      const classesToRemoveStr: string | null = entry.target.getAttribute("class-add-scroll")
+
+      const classesToAdd = classesToAddStr?.split(" ");
+      const classesToRemove = classesToRemoveStr?.split(" ");
+
+      classesToAdd?.forEach((className) => {
         entry.target.classList.remove(className);
-      });
-      entryClassesRemove.forEach((className) => {
+      })
+
+      classesToRemove?.forEach((className) => {
         entry.target.classList.add(className);
-      });
+      })
     }
   });
 });
