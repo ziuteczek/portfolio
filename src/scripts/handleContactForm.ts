d972bd2f518eaa64643281ba340e3ed0b03e1d83
Stanisław Dymny <dymnystan@gmail.com>
2025-06-12 16:47:12 +0200

    From submit popup added

--- src/scripts/handleContactForm.ts
+++ src/scripts/handleContactForm.ts
@@ -1,20 +1,32 @@
 import emailjs from "@emailjs/browser";
-import formSubmitPopup from "../components/FormSubmitPopup.html";
+import formSubmitPopupSucces from "../components/FormSubmitPopupSucces.html?raw";
+import formSubmitPopupFail from "../components/FormSubmitPopupFail.html?raw";
 emailjs.init({
   publicKey: "o5fXbnQlZ_z97Bin1",
 });
-console.log(typeof formSubmitPopup);
 
 const formEl: HTMLFormElement | null = document.querySelector(".contact-form");
 
 formEl?.addEventListener("submit", async (e) => {
+  document.body.style.cursor = "progress";
   e.preventDefault();
-
+  const popup = document.createElement("div");
   try {
-    await emailjs.sendForm("service_gm282sp", "template_38t99p9", formEl);
-    console.log("succes");
+    const response = await emailjs.sendForm(
+      "service_gm282sp",
+      "template_38t99p9",
+      formEl
+    );
     formEl?.reset();
+
+    popup.insertAdjacentHTML("afterbegin", formSubmitPopupSucces);
   } catch (err) {
     console.log(err);
+    popup.insertAdjacentHTML("afterbegin", formSubmitPopupFail);
   }
+  document.body.style.cursor = "auto";
+  document.body.append(popup);
+  setTimeout(() => {
+    popup.remove();
+  }, 5000);
 });
